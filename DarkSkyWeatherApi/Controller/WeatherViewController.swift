59d5b2d7a413aeee5bd52ea650ea63f74e268c81
Tieda Wei <weitieda@gmail.com>
2019-02-14 03:17:17 -0500

    moved UIImage related code to extension

--- DarkSkyWeatherApi/Controller/WeatherViewController.swift
+++ DarkSkyWeatherApi/Controller/WeatherViewController.swift
@@ -15,7 +15,6 @@ class WeatherViewController: UIViewController {
     let loadingFailedLabel: UILabel = {
         let l = UILabel()
         l.backgroundColor = .red
-        l.text = "Error"
         l.textAlignment = .center
         return l
     }()
@@ -47,30 +46,5 @@ class WeatherViewController: UIViewController {
         activityIndicatorView.startAnimating()
         activityIndicatorView.style = .gray
         activityIndicatorView.hidesWhenStopped = true
-    }
-    
-    func weatherIcon(of name: String) -> UIImage? {
-        switch name {
-        case "clear-day":
-            return UIImage(named: "clear-day")
-        case "clear-night":
-            return UIImage(named: "clear-night")
-        case "rain":
-            return UIImage(named: "rain")
-        case "snow":
-            return UIImage(named: "snow")
-        case "sleet":
-            return UIImage(named: "sleet")
-        case "wind":
-            return UIImage(named: "wind")
-        case "cloudy":
-            return UIImage(named: "cloudy")
-        case "partly-cloudy-day":
-            return UIImage(named: "partly-cloudy-day")
-        case "partly-cloudy-night":
-            return UIImage(named: "partly-cloudy-night")
-        default:
-            return UIImage(named: "clear-day")
-        }
     }
 }
